a5d4cbdae5f106fc251015aabc7ed86a55281812
peter-sila1 <petermunyao150@gmail.com>
2023-10-04 10:13:41 +0300

    file lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file_text:
+        file_text.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as file_text:
+        file_text.write(append_content)
 
 def read_file(file_name):
-    pass
+    
+    with open(f"{file_name}.txt", mode='r', encoding='utf-8') as file_text:
+        return file_text.read()
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
